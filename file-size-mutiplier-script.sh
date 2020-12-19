fdc5580479618ab19f0d8cd8e54525eaf64dc18f
nawazxy <ali@fidor.com>
2020-12-20 03:32:59 +0400

    Added Demo PPT

--- file-size-mutiplier-script.sh
+++ file-size-mutiplier-script.sh
@@ -1,12 +1,27 @@
+# #!/bin/bash
+
+# echo "Multiplying file size"
+# for i in {1..5};
+# do 
+#     echo "Multipling file ${i} times"
+#     cat ./NASA_access_logs >> ./NASA_access_logs_1GB; 
+# done
+
+# echo "Resulting file size is: $(ls -lrth ./NASA_access_logs_test)"
+# echo "Total number of lines in the resulting file: $(wc -l NASA_access_logs_test)"
+# # ls -lrth ./NASA_access_log_1GB
+
 #!/bin/bash
 
-echo "Multiplying file size"
-for i in {1..5};
-do 
+echo "Multipling file size"
+for i in {1..4};
+do
     echo "Multipling file ${i} times"
-    cat ./NASA_access_logs >> ./NASA_access_logs_1GB; 
+    cat /home/hadoop/dataset/nasa_logs_95.csv >> /home/hadoop/dataset/nasa_logs_1GB.csv;
 done
 
-echo "Resulting file size is: $(ls -lrth ./NASA_access_logs_test)"
-echo "Total number of lines in the resulting file: $(wc -l NASA_access_logs_test)"
-# ls -lrth ./NASA_access_log_1GB
+echo "Original file size is: $(ls -lrth ./nasa_logs_95.csv)"
+echo "Total number of lines in the original file: $(wc -l ./nasa_logs_95.csv)"
+echo "------------------------------------------------------------------------------"
+echo "Resulting file size is: $(ls -lrth ./nasa_logs_1GB.csv)"
+echo "Total number of lines in the resulting file: $(wc -l ./nasa_logs_1GB.csv)"
